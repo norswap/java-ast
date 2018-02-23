c4c166c2e6d69135574da2df87b2c1d0dc3bdd52
Nicolas "Norswap" Laurent <norswap@gmail.com>
2018-02-23 11:41:29 +0100

    make identifier chains extend expression

--- src/norswap/javaast/nodes/IdentifierChain.java
+++ src/norswap/javaast/nodes/IdentifierChain.java
@@ -3,5 +3,8 @@ package norswap.javaast.nodes;
 /**
  * Common super-interface for all identifier chains, namely {@link SimpleIdentifierChain} and
  * the two qualified versions: {@link ThisChain} and {@link SuperChain}.
+ *
+ * <p>While this interface extends {@link Expression}, an identifier chain does not always
+ * designate an expression (it can designate a type, for instance).
  */
-public interface IdentifierChain extends Node {}
+public interface IdentifierChain extends Expression {}
